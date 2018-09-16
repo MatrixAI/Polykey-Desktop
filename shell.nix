35ace5240bcb3ec7244544a14e3d366effdd7766
Roger Qiu <roger.qiu@polyhack.io>
2018-09-16 17:34:34 +1000

    WIP

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/00e56fbbee06088bf3bf82169032f5f5778588b7.tar.gz) {}
+}:
   with pkgs;
   stdenv.mkDerivation {
     name = "polykey";
-    src = ./.;
-    buildInputs = [ python2 nodejs nodePackages.node2nix ];
+    buildInputs = [ python2 nodejs flow ];
   }
