778a53ab5d1eeba0625dd14b11a3c2b49ccc6f33
Robert Cronin <robert.cronin@gatech.edu>
2020-10-26 17:38:41 +0800

    wip

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,44 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <h1>My Event</h1>
+    <p>Capacity: {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
   </div>
-  <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-// import Home from '@/views/Home.vue'; // @ is an alias to /src
-// import About from '@/views/About.vue'; // @ is an alias to /src
+<script>
+import { ref } from "vue"; // <-- Use this line if you're in a Vue 3 app
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    // Home,
-    // About,
-  },
-});
+export default {
+  setup() {
+    const capacity = ref(3);
+
+    function increaseCapacity() {
+      capacity.value++;
+    }
+    return { capacity, increaseCapacity };
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,18 +1,8 @@
-import { defineComponent } from 'vue';
-import { h, ref, reactive, createApp } from 'vue'
-// import App from './App.vue';
-// import router from './router';
-// import store from './store';
+import { createApp } from 'vue'
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
+// // this fails because the router and store both use `createRouter/createStore` methods which seem not to work
 // createApp(App).use(store).use(router).mount('#app');
-
-const App = {
-    setup() {
-      const count = ref(0)
-      const object = reactive({ foo: 'Hello World' })
-  
-      return () => h('div', [count.value, object.foo])
-    }
-  }
-
 createApp(App).mount('#app');
