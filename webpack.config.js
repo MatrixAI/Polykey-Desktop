32c514e762c84b6f5df769def0345ba72158539d
Robert Cronin <robert.cronin@gatech.edu>
2020-10-26 15:36:25 +0800

    wip

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+const env = process.env;
+
+const VueApp = {
+  mode: env.production ? "production" : "development",
+  entry: "./src/renderer/app.ts",
+
+  // This should be checked if dev mode
+  devtool: "inline-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: !env.production },
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".vue", ".json"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: "./tsconfig.json",
+        baseUrl: "./src/renderer/",
+      }),
+    ],
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+  output: {
+    filename: "renderer.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/renderer/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new WebpackBar(),
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: "true",
+      __VUE_PROD_DEVTOOLS__: "false",
+    }),
+  ],
+};
+
+const ElectronApp = {
+  target: "electron10.1-main",
+  mode: env.production ? "production" : "development",
+  entry: "./src/main/index.ts",
+  // This should be checked if dev mode
+  devtool: "inline-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: "./tsconfig.json",
+        baseUrl: "./src/main/",
+      }),
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+
+module.exports = [VueApp, ElectronApp];
