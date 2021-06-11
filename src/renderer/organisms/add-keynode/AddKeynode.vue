152b79a2fb708e76cbc11f3eb1ab34d849bb23b5
Brian Botha <brian.botha@matrix.ai>
2021-06-11 15:13:59 +1000

    WIP - Undid changes to AddKeynode.vue

--- src/renderer/organisms/add-keynode/AddKeynode.vue
+++ src/renderer/organisms/add-keynode/AddKeynode.vue
@@ -1,17 +1,24 @@
 <template>
   <div :class="['flex flex-col h-full', showDrawer ? 'block' : 'hidden']">
-    <div class="flex flex-row-reverse p-4"><Close @click="close" class="cursor-pointer" /></div>
+    <div class="flex flex-row-reverse p-4">
+      <Close class="cursor-pointer" @click="close" />
+    </div>
     <div class="flex-grow p-6">
       <div class="text-2xl font-bold">Add A Keynode</div>
       <div>
-        You can either add a Keynode by pasting the public key of a keynode or adding through a digital identity proof.
+        You can either add a Keynode by pasting the public key of a keynode or
+        adding through a digital identity proof.
       </div>
       <div class="mt-4 flex items-center">
         <div class="font-bold text-base">PUBLIC KEY</div>
         <div><Help /></div>
       </div>
       <div class="mt-2">
-        <a-textarea @input="updatePublicKey" class="rounded" placeholder="Paste public key" />
+        <a-textarea
+          class="rounded"
+          placeholder="Paste public key"
+          @input="updatePublicKey"
+        />
       </div>
       <div class="mt-6 font-bold text-base">OR ADD THROUGH</div>
       <div class="flex mt-2">
@@ -23,15 +30,19 @@
             style="width: 120px"
             @change="handleChange"
           >
-            <a-select-option value="github">
-              Github
-            </a-select-option>
+            <!--            <a-select-option value="github"> Github </a-select-option>FIXME-->
             <!-- <a-select-option value="linked">
               Linked
             </a-select-option> -->
           </a-select>
         </div>
-        <div><a-input @input="updateUsername" class="rounded" placeholder="Enter username" /></div>
+        <div>
+          <a-input
+            class="rounded"
+            placeholder="Enter username"
+            @input="updateUsername"
+          />
+        </div>
       </div>
       <div class="mt-8 flex flex-row">
         <div class="mr-3"><ClearButton @click="close">CANCEL</ClearButton></div>
@@ -39,7 +50,8 @@
       </div>
     </div>
     <div class="px-5 py-8">
-      Once a keynode trusts you, you should be able to pull secrets from their vaults quickly and easily.
+      Once a keynode trusts you, you should be able to pull secrets from their
+      vaults quickly and easily.
     </div>
   </div>
 </template>
@@ -68,7 +80,7 @@ export default defineComponent({
     Help,
     ClearButton,
     PrimaryButton,
-    MoreOutlined
+    MoreOutlined,
   },
   setup() {
     const publicKey = ref('');
@@ -86,26 +98,26 @@ export default defineComponent({
       username,
       showDrawer,
       close,
-      updatePublicKey: event => {
+      updatePublicKey: (event) => {
         publicKey.value = event.target.value;
       },
-      updateUsername: event => {
+      updateUsername: (event) => {
         username.value = event.target.value;
       },
       trust: async () => {
         try {
           if (username.value) {
             const trust = await PolykeyClient.TrustGestalt({
-              s: username.value
+              s: username.value,
             });
-            console.log(trust)
+            console.log(trust);
           }
         } catch (e) {
           console.log(e);
         }
       },
-      handleChange: () => {}
+      handleChange: () => {},
     };
-  }
+  },
 });
 </script>
