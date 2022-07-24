a3bf5c45c63b60ebdd459341232322f87ffa67b6
Roger Qiu <roger.qiu@polyhack.io>
2022-07-24 19:59:04 +1000

    WIP

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as Server from 'react-dom/server';
+
+let Greet = () => <h1>Hello, world!</h1>;
+
+console.log(Server.renderToString(<Greet />));
---
+++ test.js
@@ -0,0 +1,19 @@
+
+const { app, BrowserWindow } = require('electron');
+console.log('hello');
+console.log(app);
+
+app.on('ready', () => {
+  // const window = new BrowserWindow({
+  //   width: 800,
+  //   height: 600,
+  // });
+
+  // const window2 = new BrowserWindow({
+  //   width: 800,
+  //   height: 600,
+  // });
+
+  // window.close();
+  // app.quit();
+});
